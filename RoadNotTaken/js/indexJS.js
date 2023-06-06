c4290a2e91c3f04d8f70e0d7eb51c8f657eec312
fischeruri <63200597+fischeruri@users.noreply.github.com>
2023-06-06 23:05:58 +0300

    Add project files.

---
+++ RoadNotTaken/js/indexJS.js
@@ -0,0 +1,14 @@
+﻿function poemReveal() {
+    document.getElementById("poem").classList.add("poemRevealed");
+    document.getElementById("b1").classList.add("buttonHide");
+}
+
+function choiceReveal() {
+    document.getElementById("choice").classList.add("choiceRevealed");
+    document.getElementById("b2").classList.add("buttonHide");
+}
+
+function galleryReveal() {
+    document.getElementById("gallery").classList.add("galleryRevealed");
+    document.getElementById("b3").classList.add("buttonHide");
+}
